a8d0f9dcd0c5d1990e983c3fed7316642d5753e9
Lucas Prieto <lucasprieto30@gmail.com>
2020-12-22 23:12:04 -0300

    prueba

--- extras.py
+++ extras.py
@@ -66,7 +66,7 @@ def dameLetraApretada(key):
 
 def dibujar(screen, palabraUsuario, lista, puntos, segundos, ayuda):
     #PONE IMAGEN DE FONDO
-    fondo=pygame.image.load("LilpeepROSA1.jpg").convert()
+    fondo=pygame.image.load("LilpeepROSA.jpg").convert()
     screen.blit(fondo,[0,0])
     
     defaultFont= pygame.font.Font( "Montserrat-Italic.ttf", TAMANNO_LETRA)
